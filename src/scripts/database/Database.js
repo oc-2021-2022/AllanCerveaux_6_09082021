2c7581c78505556f11c81e72f5ba83ebb2d41d33
Callan <cerveauxallanjean@gmail.com>
2021-08-17 16:45:51 +0200

    Refactor Database and state

--- src/scripts/database/Database.js
+++ src/scripts/database/Database.js
@@ -1,23 +1,31 @@
-
 export class Database {
   constructor (dbName, dbVersion, dbUpgrade) {
     return new Promise((resolve, reject) => {
       this.db = null
-      if (!('indexedDB' in window)) reject(new Error('IndexedDB not supported !'))
+
+      if (!('indexedDB' in window)) reject(new Error('not supported'))
+
       const dbOpen = indexedDB.open(dbName, dbVersion)
+
       if (dbUpgrade) {
-        dbOpen.onupgradeneeded = event => {
-          dbUpgrade(dbOpen.result, event.oldVersion, event.newVersion)
+        dbOpen.onupgradeneeded = e => {
+          dbUpgrade(dbOpen.result, e.oldVersion, e.newVersion)
         }
       }
-      dbOpen.onsuccess = event => {
+
+      dbOpen.onsuccess = () => {
         this.db = dbOpen.result
         resolve(this)
       }
-      dbOpen.onerror = event => {
-        reject(new Error(`IndexedDB error: ${event.target.errorCode}`))
+
+      dbOpen.onerror = e => {
+        reject(new Error(`IndexedDB error: ${e.target.errorCode}`))
       }
     })
+  }
+
+  get connection () {
+    return this.db
   }
 
   set (storeName, name, value) {
@@ -32,7 +40,7 @@ export class Database {
       }
 
       transaction.onerror = () => {
-        reject(new Error(transaction.error))
+        reject(transaction.error)
       }
     })
   }
@@ -41,11 +49,14 @@ export class Database {
     return new Promise((resolve, reject) => {
       const transaction = this.db.transaction(storeName, 'readonly')
       const store = transaction.objectStore(storeName)
-
       const request = store.get(name)
+      request.onsuccess = () => {
+        resolve(request.result)
+      }
 
-      request.oncomplete = (event) => resolve(request.result)
-      request.onerror = event => reject(request.error)
+      request.onerror = () => {
+        reject(request.error)
+      }
     })
   }
 }
--- src/scripts/database/State.js
+++ src/scripts/database/State.js
@@ -1,20 +1,18 @@
 import { Database } from './Database'
-
 export class State {
-  static dbName = 'fisheye';
-  static dbVersion = 1;
-  static storeName = 'photographers';
-  static DB = null;
-  static target = new EventTarget();
+  static dbName = 'stateDB'
+  static dbVersion = 1
+  static storeName = 'state'
+  static DB = null
+  static target = new EventTarget()
 
-  constructor(observed, updateCallback) {
-    this.updateCallback = updateCallback;
+  constructor (observed, updateCallback) {
+    this.updateCallback = updateCallback
+    this.observed = new Set(observed)
 
-    this.observed = new Set(observed);
-
-    State.target.addEventListener('set', event => {
-      if (this.updateCallback && this.observed.has(event.detail.name)) {
-        this.updateCallback(event.detail.name, event.detail.value)
+    State.target.addEventListener('set', e => {
+      if (this.updateCallback && this.observed.has( e.detail.name )) {
+        this.updateCallback(e.detail.name, e.detail.value)
       }
     })
   }
@@ -25,34 +23,32 @@ export class State {
       State.dbVersion,
       (db, oldVersion, newVersion) => {
         switch (oldVersion) {
-          case 0:
-            db.createObjectStore(State.storeName)
-            break;
-        
-          default:
-            break;
+          case 0: {
+            db.createObjectStore( State.storeName )
+          }
         }
-      }
-    )
+
+      })
 
     return State.DB
   }
 
-  async set(name, value) {
-    console.log(name, value)
-    this.observed.add(name)
-    const db = await this.dbConnect()
-    await db.set(State.storeName, name, value);
 
+  async set (name, value) {
+    this.observed.add(name)
+
+    const db = await this.dbConnect()
+    await db.set( State.storeName, name, value )
+    
     const event = new CustomEvent('set', { detail: { name, value } })
     State.target.dispatchEvent(event)
+
   }
 
-  async get(name) {
+  async get (name) {
     this.observed.add(name)
 
     const db = await this.dbConnect()
-    return await db.get(State.storeName, name);
-
+    return await db.get( State.storeName, name )
   }
 }
