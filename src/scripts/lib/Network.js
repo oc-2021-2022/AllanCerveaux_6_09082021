c45957d3ce955af0c6cf1aed4cd057efffba6189
Callan <cerveauxallanjean@gmail.com>
2021-09-02 09:38:00 +0200

    Fix: ESLINT, and return data in `fetchByName` method

--- src/scripts/lib/Network.js
+++ src/scripts/lib/Network.js
@@ -72,11 +72,11 @@ export class Network {
         if (await this.testRemoteAPI()) {
           return data[name]
         }
-        return data
+        return data[name]
       })
       .catch(this.error)
   }
-  
+
   /**
    * Return error
    * @param {Error} err
