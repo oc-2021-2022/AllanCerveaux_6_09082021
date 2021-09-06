d16dce3ff1841d5ede5e2fa0bb66e528ec0a3848
Callan <cerveauxallanjean@gmail.com>
2021-09-06 17:26:16 +0200

    UPDATE: Sort data by name TODO: Make a query for sorting

--- src/scripts/components/filters/filter-service.js
+++ src/scripts/components/filters/filter-service.js
@@ -5,19 +5,18 @@ export class FilterService {
     this.query = new Query()
   }
 
-  async sortByTags (data, ...tags) {
+  async sortByTagsName (data, ...tags) {
+    if (!tags.length || tags[0] === null || tags[0] === 'undefined') return data
 
+    return await this.query.run(data, {
+      tags: { $in: tags }
+    })
   }
 
   async sortByName (data, type) {
-
-  }
-
-  async sortByLike (data, type) {
-
-  }
-
-  async sortByDate (data, type) {
-
+    return await data.sort((current, next) => {
+      if (type.toLowerCase() === 'asc') return current.name.localeCompare(next.name) === 1
+      else if (type.toLowerCase() === 'desc') return current.name.localeCompare(next.name) === -1
+    })
   }
 }
