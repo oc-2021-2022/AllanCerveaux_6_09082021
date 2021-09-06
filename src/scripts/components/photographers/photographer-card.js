b8dd03ec4c5db8bdcdaf96aa093326a6e78a667f
Callan <cerveauxallanjean@gmail.com>
2021-09-06 16:42:30 +0200

    UPDATE: Stateless component just display photographer info

--- src/scripts/components/photographers/photographer-card.js
+++ src/scripts/components/photographers/photographer-card.js
@@ -1,24 +1,36 @@
 
 export class PhotographerCard extends HTMLElement {
   static get observedAttributes () {
-    return []
+    return ['photographer']
   }
 
   constructor (data) {
     super()
+    this.image_path = './static/images'
   }
 
-  attributeChangedCallback () {
-
-  }
-
-  async connectedCallback () {
+  connectedCallback () {
     this.shadow = this.attachShadow({ mode: 'closed' })
+    this.photographer = JSON.parse(this.getAttribute('photographer'))
+    this.render()
   }
 
   render () {
-    return /* html */`
-      <article></article>
+    this.shadow.innerHTML = /* html */`
+      <article class="card">
+        <div class="card-header">
+          <img src="${this.image_path}/portraits/${this.photographer.portrait}" width="250"/>
+          <h3>${this.photographer.name}</h3>
+        </div>
+        <div class="card-content">
+          <span>${this.photographer.city}, ${this.photographer.country}</span>
+          <p>${this.photographer.tagline.replace('&', "'")}</p>
+          <span>${this.photographer.price}</span>
+        </div>
+        <div class="card-footer">
+          ${this.photographer.tags.map(tag => /* html */`<span>#${tag}</span>`).join(' ')}
+        <div>
+      </article>
     `
   }
 }
