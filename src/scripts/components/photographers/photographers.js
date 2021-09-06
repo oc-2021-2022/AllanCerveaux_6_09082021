0f316ab8bb5b0f076f67a6046a88c4b0a9adde1a
Callan <cerveauxallanjean@gmail.com>
2021-09-06 16:41:17 +0200

    UPDATE: updates the list of photographers according to the selected filter

--- src/scripts/components/photographers/photographers.js
+++ src/scripts/components/photographers/photographers.js
@@ -1,26 +1,39 @@
+import { FilterService } from '../filters'
+
 export class Photographers extends HTMLElement {
   static get observedAttributes () {
-    return []
+    return ['photographers', 'filter']
   }
 
   constructor (data) {
     super()
+    this.filter_service = new FilterService()
   }
 
-  attributeChangedCallback (attrName, oldValue, newValue) {
+  async attributeChangedCallback (attrName, oldValue, newValue) {
     if (oldValue !== newValue) {
       this[attrName] = newValue
+      await this.updateCardList(this.filter)
     }
   }
 
   async connectedCallback () {
     this.shadow = this.attachShadow({ mode: 'closed' })
+    await this.updateCardList()
+    this.render()
+  }
+
+  async updateCardList(filter = null) {
+    const filteredPhotographers = await this.filter_service.sortByTagsName(JSON.parse(this.photographers), filter)
+    this.cardList = ``
+    filteredPhotographers.forEach(photographer => {
+      this.cardList += /* html */`<photographer-card photographer='${JSON.stringify(photographer)}'></photographer-card>`
+    })
+    this.render()
   }
 
   render () {
-    return /* html */`
-      <section></section>
-    `
+    this.shadow.innerHTML = /* html */`${this.cardList}`
   }
 }
 
