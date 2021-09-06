5142946e9566a25ddaa91719c9d83691072d7f94
Callan <cerveauxallanjean@gmail.com>
2021-09-06 16:24:05 +0200

    Update: remove usless constructor

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -1,11 +1,6 @@
 import '../lib/router/RouterView'
 
 export class App extends HTMLElement {
-  constructor () {
-    super()
-    this.appName = 'FishEye'
-  }
-
   connectedCallback () {
     this.shadow = this.attachShadow({ mode: 'closed' })
     this.shadow.innerHTML = this.render()
@@ -14,7 +9,7 @@ export class App extends HTMLElement {
   render () {
     return /* html */`
       <main>
-        <h1>Hello ${this.appName}</h1>
+        <img src="./images/logo.svg" alt=""/>
         <router-view></router-view>
       </main>
     `
