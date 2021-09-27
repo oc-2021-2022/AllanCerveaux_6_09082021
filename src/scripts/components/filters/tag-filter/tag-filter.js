8f1b5fabff16a4779222999df631f30cdd2046bf
Callan <cerveauxallanjean@gmail.com>
2021-09-27 10:55:40 +0200

    FIX: Renaming option tag

--- src/scripts/components/filters/tag-filter/tag-filter.js
+++ src/scripts/components/filters/tag-filter/tag-filter.js
@@ -35,7 +35,7 @@ class TagFilter extends HTMLElement {
   async tagList () {
     return await JSON.parse(this.filter_data).map(tag => this.type === 'tag'
       ? /* html */`<a class="tag" href="" aria-label="${tag}">#${tag}</a>`
-      : /* html */`<option class="tag" value="${tag}">#${tag}</option>`)
+      : /* html */`<option class="tag" value="${tag}">${tag}</option>`)
       .join(' ')
   }
 
