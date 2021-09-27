880c1d9042d91582f0087e5a9c9a05826ac89696
Callan <cerveauxallanjean@gmail.com>
2021-09-27 10:59:29 +0200

    UPDATE: export photographer component

--- src/scripts/components/photographers/index.js
+++ src/scripts/components/photographers/index.js
@@ -1,3 +1,4 @@
 export { PhotographerCard } from './card/photographer-card'
 export { PhotographerService } from './photographer-service'
 export { Photographers } from './photographers'
+export { PhotographerProfile } from './profile/photographer-profile'
