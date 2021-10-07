08a41b61883cd0c5419a55d4ecb1562ac0376e4a
Callan <cerveauxallanjean@gmail.com>
2021-10-07 15:17:38 +0200

    Update: add some method to filter data

--- src/scripts/components/filters/filter-service.js
+++ src/scripts/components/filters/filter-service.js
@@ -15,8 +15,23 @@ export class FilterService {
 
   async sortByName (data, type) {
     return await data.sort((current, next) => {
-      if (type.toLowerCase() === 'asc') return current.name.localeCompare(next.name) === 1
-      else if (type.toLowerCase() === 'desc') return current.name.localeCompare(next.name) === -1
+      if (type.toLowerCase() === 'asc') return (current.name ?? current.title).localeCompare(next.name) === 1
+      else if (type.toLowerCase() === 'desc') return (current.name ?? current.title).localeCompare(next.name) === -1
+    })
+  }
+
+  async greaterThan (data) {
+    return await data.sort((current, next) => current.likes < next.likes)
+  }
+
+  async lowerThan (data) {
+    return await data.sort((current, next) => current.likes > next.likes)
+  }
+
+  async sortByDate (data, type) {
+    return await data.sort((current, next) => {
+      if (type.toLowerCase() === 'asc') return new Date(next.date) - new Date(current.date)
+      else if (type.toLowerCase() === 'desc') return new Date(current.date) - new Date(next.date)
     })
   }
 }
