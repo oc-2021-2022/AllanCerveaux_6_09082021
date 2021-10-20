96a338ffa0e54fc20e3f0e04995fef3827ed751b
Callan <cerveauxallanjean@gmail.com>
2021-10-20 18:33:42 +0200

    Update: display information into console

--- src/scripts/components/photographers/contact/contact.js
+++ src/scripts/components/photographers/contact/contact.js
@@ -17,10 +17,11 @@ export class Contact extends Component {
     this.remove()
   }
 
-  sendMail (event) {
+  sendMail = (event) => {
     event.preventDefault()
     const data = new FormData(event.target)
-    data.forEach((value, name) => {})
+    data.forEach((value, name) => console.log(name, value))
+    this.remove()
   }
 
   render () {
@@ -36,19 +37,19 @@ export class Contact extends Component {
         <form class="contact-form">
         <div class="form-control">
           <label>Prénom</label>
-          <input type="text" name="first_name"/>
+          <input type="text" name="first_name" value="Allan"/>
         </div>
           <div class="form-control">
             <label>Nom</label>
-            <input type="text" name="last_name"/>
+            <input type="text" name="last_name" value="Cerveaux"/>
           </div>
           <div class="form-control">
             <label>Email</label>
-            <input type="email" name="email"/>
+            <input type="email" name="email" value="cerveauxallanjean@gmail.com"/>
           </div>
           <div class="form-control">
             <label>Message</label>
-            <textarea name="message"></textarea>
+            <textarea name="message">Duis dolor minim cupidatat in ipsum sint duis ullamco elit aliquip sit officia veniam. Culpa cillum ut sint mollit labore nisi ad duis ex eiusmod velit sit. Sint consectetur Lorem id occaecat. Est sint non duis sit cupidatat dolore incididunt. Excepteur elit est aliquip eiusmod culpa.</textarea>
           </div>
           <div class="form-control">
             <button>Envoyer</button>
