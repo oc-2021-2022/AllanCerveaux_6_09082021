2c490ec031f521238f3d2ede701f904ad71987d7
Callan <cerveauxallanjean@gmail.com>
2021-10-20 18:29:08 +0200

    Update : add select-tag

--- src/scripts/components/filters/tag-filter/tag-filter.js
+++ src/scripts/components/filters/tag-filter/tag-filter.js
@@ -1,6 +1,6 @@
 import stylesheet from 'bundle-text:./_tag_filter.scss'
 import { Component } from '../../../lib/Component'
-
+import '../select-tag/select-tag'
 class FilterComponent extends Component {
   styles = stylesheet
   static get observedAttributes () {
@@ -10,8 +10,8 @@ class FilterComponent extends Component {
   generatedFilterList () {
     if (this.filter_data) {
       return JSON.parse(this.filter_data).map(tag => this.type === 'tag'
-        ? /* html */`<a class="tag tag-button" href="" aria-label='${tag}'>#${tag}</a>`
-        : /* html */`<option class="tag tag-option" value='${tag}'>${tag}</option>`)
+        ? /* html */`<a class="tag tag-button" href="" aria-label='${tag}' data-tag="${tag}">#${tag}</a>`
+        : /* html */`<li class="tag tag-option" data-tag="${tag}">${tag}</li>`)
         .join(' ')
     }
   }
@@ -19,7 +19,7 @@ class FilterComponent extends Component {
   template () {
     if (this.type === 'select') {
       return /* html */`
-        <select>${this.generatedFilterList()}</select>
+        <select-tag options='${this.generatedFilterList()}'></select-tag>
       `
     } else if (this.type === 'tag') {
       return this.generatedFilterList()
@@ -28,21 +28,23 @@ class FilterComponent extends Component {
 
   setEvents () {
     this.tags = this.shadow.querySelectorAll('.tag')
-    this.tags.forEach(this.handleClick)
+    this.tags.forEach(tag => tag.addEventListener('click', (event) => this.handleClick(event, tag)))
+    if (this.shadow.querySelector('select-tag') !== null) this.shadow.querySelector('select-tag').addEventListener('select-get-tag', this.handleClick)
   }
 
-  handleClick = async (tag) => {
-    tag.addEventListener('click', (event) => {
-      event.preventDefault()
+  handleClick = (event, link = null) => {
+    event.preventDefault()
+    if (link) {
       const tags = Array.from(this.tags).filter(tag => tag.classList.contains('active'))
-      if (!tag.classList.contains('active')) {
-        tag.className += ' active'
+      if (!link.classList.contains('active')) {
+        link.className += ' active'
       }
       tags.forEach(tag => {
         tag.className = 'tag tag-button'
       })
-      this.dispatchEvent(new CustomEvent('selected-tag', { bubbles: true, detail: { tag: event.target.textContent.replace('#', '') } }))
-    })
+    }
+    const tag = event.target.getAttribute('data-tag') ?? event.detail.getAttribute('data-tag')
+    this.dispatchEvent(new CustomEvent('selected-tag', { bubbles: true, detail: { tag } }))
   }
 
   render () {
